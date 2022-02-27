e1789a50bbf4cfbff2d3dd33bc6412c880d24fd0
Dharwin R V J <dharwinruppa1422@gmail.com>
2022-02-27 09:28:19 +0530

    dwin-mart

--- config/database.js
+++ config/database.js
@@ -1,3 +1,3 @@
 module.exports = {
-  database: "mongodb+srv://dharwin:rupparam@dharwin.wkbz4.mongodb.net/dmart?retryWrites=true&w=majority",
+  database: "mongodb+srv://dharwinrvj:rupparam@dharwin.wkbz4.mongodb.net/dmart?retryWrites=true&w=majority",
 };
